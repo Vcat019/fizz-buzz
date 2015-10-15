68b6b967fc697b0983ef66d2cb27840a4c7ba680
vcat019 <vcat019@gmail.com>
2015-10-14 20:03:27 -0400

    final commit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,53 @@
-$(document).ready(function() {
-	$('button').click(function(){
-	for ( var i = 1; i <= 100; i++)  
-	if	(i % 3 === 0 && i % 5 == 0)  {
-	$('ul').append('<li class="fizz-buzz">' + 'FizzBuzz' + '</li>');
-	$('div').hide("");
-	$('input').val("");
+function getNumber() { 
+	var yourNumber = parseInt(prompt("Enter a number"));
+	if (isNaN(yourNumber)) {
+		alert("Please enter a whole number");
+		return false;
 
-} else if ( i % 5 === 0) {
-	$('ul').append('<li class="buzz">' + 'Buzz' + '</li>');
-	$('div').hide("");
-	$('input').val("");
+	}
+	else {
+		return yourNumber;
+
+	}
+}
+
+
+
+
+         function fizzBuzz(number) {
+	 for ( var i = 1; i <= number; i++)  {
+		if	(i % 15 == 0)  {
+			//$('ul').append('<li class="fizz-buzz">' + 'FizzBuzz' + '</li>');
+	document.write('FizzBuzz'+"<br />");
+
+} else if ( i % 5 == 0) {
+	//$('ul').append('<li class="buzz">' + 'Buzz' + '</li>');
+	document.write('Buzz'+"<br />");
 } else if 
-	( i % 3  === 0) {
-$('ul').append('<li class="fizz">' + 'Fizz'+'</li>' );
-$('div').hide();
-$('input').val("");
+	( i % 3  == 0) {
+//$('ul').append('<li class="fizz">' + 'Fizz'+'</li>' );
+document.write('Fizz' + "<br />")
 }
 
 
 else {
-	$('ul').append('<li>' + i + '</li>');
-		$('input').val("");
-		$('div').hide();
-		};
-	});
+	//$('ul').append('<li>' + i + '</li>');
+document.write(i + "<br />")
+	
+			};
+		}
+	}
+$(document).ready(function() {
+var yourNumber = false;
+while(yourNumber == false) {
+yourNumber = getNumber();
+}
+fizzBuzz(yourNumber);
 });
+      
+
+
+
+
 
 
